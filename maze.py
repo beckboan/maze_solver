7345c973f712bfbf77695d95a5c7365c221bce2f
Beck Boan <beckmboan@gmail.com>
2024-05-15 20:14:12 -0400

    Changed to rows [i] and cols [j]

--- maze.py
+++ maze.py
@@ -21,16 +21,22 @@ class Maze:
         self.size_y = size_y
         self._win = win
         self._cells = []
-        self._animate_time = 0.25
+        self._animate_time = 0.1
+
+        if seed:
+            random.seed(seed)
+
         self._create_cells()
         self._open_start_end()
         self._break_walls_r(0, 0)
+        self._reset_all_visited()
+        # self._solve_r(0, 0)
 
     def _create_cells(self):
-        self._cells = [[Cell(self._win) for i in range(self.rows)]
-                       for j in range(self.cols)]
-        for i in range(self.cols):
-            for j in range(self.rows):
+        self._cells = [[Cell(self._win) for j in range(self.cols)]
+                       for i in range(self.rows)]
+        for i in range(self.rows):
+            for j in range(self.cols):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
@@ -46,8 +52,8 @@ class Maze:
     def _open_start_end(self):
         self._cells[0][0].walls[0] = 0
         self._draw_cell(0, 0)
-        self._cells[self.cols-1][self.rows-1].walls[2] = 0
-        self._draw_cell(self.cols-1, self.rows-1)
+        self._cells[self.rows-1][self.cols-1].walls[2] = 0
+        self._draw_cell(self.rows-1, self.cols-1)
 
     def _animate(self):
         if self._win is None:
@@ -58,8 +64,7 @@ class Maze:
     def _break_walls_r(self, i, j):
         self._cells[i][j]._visited = True
 
-        print(i, j)
-
+        # Up, Right, Down, Left
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         while True:
             adjacent_cells = []
@@ -67,10 +72,9 @@ class Maze:
 
             for dc, dr in directions:
                 ni, nj = i + dc, j + dr
-                if 0 <= ni < self.cols and 0 <= nj < self.rows and not self._cells[ni][nj]._visited:
+                if 0 <= ni < self.rows and 0 <= nj < self.cols and not self._cells[ni][nj]._visited:
                     adjacent_cells.append((ni, nj))
 
-            print(adjacent_cells)
             if not adjacent_cells:
                 return
 
@@ -78,23 +82,31 @@ class Maze:
             ni = adjacent_cells[choice][0]
             nj = adjacent_cells[choice][1]
 
+            # print(ni, nj)
+
             if ni == i-1:
                 self._cells[i][j].walls[0] = 0
                 self._cells[ni][nj].walls[2] = 0
-                print("Up")
             elif nj == j+1:
                 self._cells[i][j].walls[1] = 0
                 self._cells[ni][nj].walls[3] = 0
-                print("Right")
             elif ni == i+1:
                 self._cells[i][j].walls[2] = 0
                 self._cells[ni][nj].walls[0] = 0
-                print("Down")
             elif nj == j-1:
                 self._cells[i][j].walls[3] = 0
                 self._cells[ni][nj].walls[1] = 0
-                print("Left")
 
             self._draw_cell(i, j)
 
             self._break_walls_r(ni, nj)
+
+    def _reset_all_visited(self):
+        for col in self._cells:
+            for cell in col:
+                cell._visited = False
+
+    def _solve_r(self, i, j):
+        self._animate()
+
+        return False
